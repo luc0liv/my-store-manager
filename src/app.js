c395bb233228aa4aab2932d4e2cbbd25f14d6787
luc0liv <luoliveiradacunha@gmail.com>
2023-03-23 20:46:13 -0300

    feat: create sales post endpoint

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const productsRouter = require('./routers/productsRouter');
+const salesRouter = require('./routers/salesRouter');
 const errorHandler = require('./middlewares/errorHandler');
 
 const app = express();
 
 app.use(express.json());
 app.use('/products', productsRouter);
+app.use('/sales', salesRouter);
 app.use(errorHandler);
 
 // não remova esse endpoint, é para o avaliador funcionar
